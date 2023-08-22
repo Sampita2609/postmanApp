5f7d4807c8cf4a837e12acfda194a8e84c871eb9
sampita26 <dassampita123@gmail.com>
2023-08-22 15:46:29 -0300

    Initial Commit

--- serverApi.js
+++ serverApi.js
@@ -3,7 +3,7 @@ var bodyParser = require("body-parser");
 var app = express();
 app.use(express.json());
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, OPTIONS, PUT, POST, DELETE");
   res.header(
     "Access-Control-Allow-Headers",
